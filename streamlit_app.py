fe05d1097e064eeb4479a0b182aed174f1234e03
GenTpham <58386839+GenTpham@users.noreply.github.com>
2024-11-12 22:52:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ st.title('🎁 Machine Learning App')
 st.info('This is app builds a machine learning model!')
 
 with st.expander('Data'):
-  st.write('**Raw data**')
+  st.write('**Raw Data**')
   df = pd.read_csv('https://raw.githubusercontent.com/GenTpham/RandomForest/refs/heads/main/weather.csv')
   df
