1493c58ac7ab36f04b47557aabd20caaaa5cf3c1
GenTpham <58386839+GenTpham@users.noreply.github.com>
2024-11-15 09:06:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,8 +70,14 @@ clf.fit(X, y)
 prediction = clf.predict(input_row)
 prediction_proba = clf.predict_proba(input_row)
 
-prediction_proba.columns = ['drizzle', 'fog', 'rain', 'snow', 'sun']
-
+df_prediction_proba = pd.DataFrame(prediction_proba)
+df_prediction_proba.columns = ['drizzle', 'fog', 'rain', 'snow', 'sun']
+df_prediction_proba.rename(columns = {'drizzle': 0,
+                                        'fog': 1,
+                                        'rain': 2,
+                                        'snow': 3,
+                                        'sun': 4})
+df_prediction_proba
 
 
 
